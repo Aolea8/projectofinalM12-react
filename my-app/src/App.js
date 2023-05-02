5a4214e4df235c9c28fea7f48510f369ce2e0247
Olea <arnauolea@gmail.com>
2023-05-02 21:57:09 +0200

    subidita pal miercoles

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,31 @@
+import 'swiper/swiper.min.css';
+import './assets/boxicons-2.0.7/css/boxicons.min.css';
 import './App.scss';
+
+import { Routes, Route } from "react-router-dom";
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import Home from './pages/Home';
+import Catalog from './pages/Catalog';
+import Detail from './pages/Detail';
 
 function App() {
     return (
-        <div>
-            App
-        </div>
-    );
+        <>
+        <Header/>
+        <Routes>
+            
+            <Route path="/" element={<Home />}/>
+            
+                <Route path=":category/search/:keyword"  element={<Catalog />}/>
+                <Route path=':category/:id' element={<Detail/>} />
+                <Route path='/:category' element={<Catalog/>} />
+                
+            
+        </Routes>
+        <Footer/>
+        </>
+    )
 }
 
 export default App;
